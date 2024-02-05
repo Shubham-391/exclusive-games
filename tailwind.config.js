b62fdc80e20a3597320828c2ba054045240b48eb
Shubham-391 <subhamy1234@gmail.com>
2024-02-06 01:30:57 +0530

    headerbg

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
     },
     backgroundImage: {
       headerbg:
-        "url('/src/assets/images/headerbg.webp'), linear-gradient(180deg, rgba(0,0,0,0)89.07%, #00141B 97.5%)",
+        "linear-gradient(180deg, rgba(0,0,0,0)89.07%, #00141B 97.5%), url('/src/assets/images/headerbg.webp')",
       button2bg: "linear-gradient(306.99deg, #51C8EF -13.72%, #7AF57A 102.02%)",
       button2bg2:
         "linear-gradient(306.99deg, #51C8EF -13.72%, #FFFFFF 102.02%)",
